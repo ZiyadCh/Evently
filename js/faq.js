a4067a412af2a0004b33e20ddee3ac342c3aa056
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2025-10-24 15:10:57 +0100

    Added the validation js

--- js/faq.js
+++ js/faq.js
@@ -23,4 +23,30 @@ document.querySelectorAll('.footer-title').forEach(title => {
 
 
 
+const form = document.getElementById('contact-form');
+const error = document.getElementById('error');
+const success = document.getElementById('success');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const message = document.getElementById('message').value.trim();
+
+  if (name === "" || email === "" || message === "") {
+    error.style.display = "block";
+    error.textContent = "Fill all the input";
+    return;
+  }
+
+  if (!email.includes("@")){
+    error.style.display = "block";
+    error.textContent = "Email invalide.";
+    return;
+  }
+
+  success.style.display = "block";
+  success.textContent = "Message envoye avec succes";
+  form.reset();
+})
